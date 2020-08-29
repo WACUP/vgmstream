619669dc062cfbe36c941996a36d372d20fd8b20
Darren Owen <winamp.plugins@gmail.com>
2020-08-29 02:29:27 +0100

    Merge remote-tracking branch 'upstream/master' into master

--- wacup/in_vgmstream.c
+++ wacup/in_vgmstream.c
@@ -54,10 +54,10 @@
 #endif
 
 #ifndef VERSIONW
-#define VERSIONW L"2.1.3234"
+#define VERSIONW L"2.1.3237"
 #endif
 
-#define LIBVGMSTREAM_BUILD "1050-3124-g573e3bd0-wacup"
+#define LIBVGMSTREAM_BUILD "1050-3237-g87fc7007-wacup"
 #define APP_NAME "vgmstream plugin"
 #define PLUGIN_DESCRIPTION "vgmstream Decoder v" VERSION
 #define PLUGIN_DESCRIPTIONW L"vgmstream Decoder v" VERSIONW
